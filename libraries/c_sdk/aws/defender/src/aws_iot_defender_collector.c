3ef5ecd7fa4c51c0e500ed7d6ea446d3ef1d7632
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2019-07-15 11:44:50 -0700

    heap metrics.

--- libraries/c_sdk/aws/defender/src/aws_iot_defender_collector.c
+++ libraries/c_sdk/aws/defender/src/aws_iot_defender_collector.c
@@ -37,47 +37,55 @@
 #include "task.h"
 
 /* Used in debugging. It will decode the report with cbor format and print to stdout. */
-#define DEBUG_CBOR_PRINT               0
+#define DEBUG_CBOR_PRINT                0
 
-#define  HEADER_TAG                    AwsIotDefenderInternal_SelectTag( "header", "hed" )
-#define  REPORTID_TAG                  AwsIotDefenderInternal_SelectTag( "report_id", "rid" )
-#define  VERSION_TAG                   AwsIotDefenderInternal_SelectTag( "version", "v" )
-#define  VERSION_1_0                   "1.0" /* Used by defender service to indicate the schema change of report, e.g. adding new field. */
-#define  VERSION_1_1                   "1.1"
-#define  METRICS_TAG                   AwsIotDefenderInternal_SelectTag( "metrics", "met" )
+/* Long tags are not recommended to be used. */
+#define  HEADER_TAG                     AwsIotDefenderInternal_SelectTag( "header", "hed" )
+#define  REPORTID_TAG                   AwsIotDefenderInternal_SelectTag( "report_id", "rid" )
+#define  VERSION_TAG                    AwsIotDefenderInternal_SelectTag( "version", "v" )
+#define  VERSION_1_0                    "1.0" /* Used by defender service to indicate the schema change of report, e.g. adding new field. */
+#define  VERSION_1_1                    "1.1"
+#define  METRICS_TAG                    AwsIotDefenderInternal_SelectTag( "metrics", "met" )
 
 /* Network metrics -- TCP connections. */
-#define TCP_CONN_TAG                   AwsIotDefenderInternal_SelectTag( "tcp_connections", "tc" )
-#define EST_CONN_TAG                   AwsIotDefenderInternal_SelectTag( "established_connections", "ec" )
-#define TOTAL_TAG                      AwsIotDefenderInternal_SelectTag( "total", "t" )
-#define CONN_TAG                       AwsIotDefenderInternal_SelectTag( "connections", "cs" )
-#define REMOTE_ADDR_TAG                AwsIotDefenderInternal_SelectTag( "remote_addr", "rad" )
+#define TCP_CONN_TAG                    AwsIotDefenderInternal_SelectTag( "tcp_connections", "tc" )
+#define EST_CONN_TAG                    AwsIotDefenderInternal_SelectTag( "established_connections", "ec" )
+#define TOTAL_TAG                       AwsIotDefenderInternal_SelectTag( "total", "t" )
+#define CONN_TAG                        AwsIotDefenderInternal_SelectTag( "connections", "cs" )
+#define REMOTE_ADDR_TAG                 AwsIotDefenderInternal_SelectTag( "remote_addr", "rad" )
 
 /* Kernel metrics.
  * Prefixed with k (kernel), m (mcu), t (task), hp (hp), st (stack). */
-#define KERNEL_METIRCS                 AwsIotDefenderInternal_SelectTag( "kernel_metrics", "km" )
-#define KERNEL_MCU_UPTIME              AwsIotDefenderInternal_SelectTag( "mcu_uptime", "mupt" )
-#define KERNEL_MCU_UTILIZATION         AwsIotDefenderInternal_SelectTag( "mcu_utilization", "mpct" )
-#define KERNEL_FREE_HEAP_SIZE          AwsIotDefenderInternal_SelectTag( "free_heap_size", "hp" )
-#define KERNEL_HEAP_LOW_WATERMARK      AwsIotDefenderInternal_SelectTag( "heap_low_watermark", "hplo" )
-#define KERNEL_NUM_OF_TASKS            AwsIotDefenderInternal_SelectTag( "num_of_tasks", "tno" )
-#define KERNEL_TASK_DETAILS            AwsIotDefenderInternal_SelectTag( "task_details", "tdet" )
+#define KERNEL_METIRCS                  AwsIotDefenderInternal_SelectTag( "kernel_metrics", "km" )
+#define KERNEL_MCU_UPTIME               AwsIotDefenderInternal_SelectTag( "mcu_uptime", "mupt" )
+#define KERNEL_MCU_UTILIZATION          AwsIotDefenderInternal_SelectTag( "mcu_utilization", "mpct" )
 
-#define KERNEL_TASK_ID                 AwsIotDefenderInternal_SelectTag( "task_id", "tid" )
-#define KERNEL_TASK_NAME               AwsIotDefenderInternal_SelectTag( "task_name", "tn" )
-#define KERNEL_TASK_STATUS             AwsIotDefenderInternal_SelectTag( "task_status", "ts" )
-#define KERNEL_TASK_PRIORITY           AwsIotDefenderInternal_SelectTag( "task_priority", "tp" )
-#define KERNEL_TASK_ABS_CYCLES         AwsIotDefenderInternal_SelectTag( "task_abs_cycles", "tcyc" )
-#define KERNEL_TASK_PERCENTAGE         AwsIotDefenderInternal_SelectTag( "task_percentage", "tpct" )
-#define KERNEL_STACK_HIGH_WATERMARK    AwsIotDefenderInternal_SelectTag( "stack_high_watermark", "sthi" )
+#define KERNEL_HEAP_FREE_SIZE           AwsIotDefenderInternal_SelectTag( "heap_free_size", "hp" )
+#define KERNEL_HEAP_LARGEST_FREE        AwsIotDefenderInternal_SelectTag( "heap_largest_free_block", "hplg" )
+#define KERNEL_HEAP_SMALLEST_FREE       AwsIotDefenderInternal_SelectTag( "heap_smallest_free_block", "hpsm" )
+#define KERNEL_HEAP_NUM_OF_FREE         AwsIotDefenderInternal_SelectTag( "heap_free_blocks", "hpno" )
+#define KERNEL_HEAP_LOW_WATERMARK       AwsIotDefenderInternal_SelectTag( "heap_low_watermark", "hplo" )
+#define KERNEL_HEAP_SUCCESSFUL_ALLOC    AwsIotDefenderInternal_SelectTag( "heap_succ_alloc", "hpal" )
+#define KERNEL_HEAP_SUCCESSFUL_FREE     AwsIotDefenderInternal_SelectTag( "heap_succ_free", "hpfr" )
+
+#define KERNEL_NUM_OF_TASKS             AwsIotDefenderInternal_SelectTag( "num_of_tasks", "tno" )
+#define KERNEL_TASK_DETAILS             AwsIotDefenderInternal_SelectTag( "task_details", "tdet" )
+
+#define KERNEL_TASK_ID                  AwsIotDefenderInternal_SelectTag( "task_id", "tid" )
+#define KERNEL_TASK_NAME                AwsIotDefenderInternal_SelectTag( "task_name", "tn" )
+#define KERNEL_TASK_STATUS              AwsIotDefenderInternal_SelectTag( "task_status", "ts" )
+#define KERNEL_TASK_PRIORITY            AwsIotDefenderInternal_SelectTag( "task_priority", "tp" )
+#define KERNEL_TASK_ABS_CYCLES          AwsIotDefenderInternal_SelectTag( "task_abs_cycles", "tcyc" )
+#define KERNEL_TASK_PERCENTAGE          AwsIotDefenderInternal_SelectTag( "task_percentage", "tpct" )
+#define KERNEL_STACK_HIGH_WATERMARK     AwsIotDefenderInternal_SelectTag( "stack_high_watermark", "sthi" )
 
 /* Device type.
  * Prefixed with d (device). */
-#define DEVICE_TYPE                    AwsIotDefenderInternal_SelectTag( "device_type", "dtp" )
+#define DEVICE_TYPE                     AwsIotDefenderInternal_SelectTag( "device_type", "dtp" )
 
 /* Return value for _taskNameFilterIdle(). */
-#define TASK_NAME_EQUAL                ( 1 )
-#define TASK_NAME_NOT_EQUAL            ( 0 )
+#define TASK_NAME_EQUAL                 ( 1 )
+#define TASK_NAME_NOT_EQUAL             ( 0 )
 
 /**
  * Structure to hold a metrics report.
@@ -486,8 +494,10 @@ static void _serializeKernelRuntimeStats( void * param1 )
     uint32_t ulIdleTime = 0;
 
     TaskStatus_t * pxTaskStatusArray = pvPortMalloc( uxArraySize * sizeof( TaskStatus_t ) );
+    HeapStats_t * pxHeapStatus = pvPortMalloc( sizeof( HeapStats_t ) );
 
     uxArraySize = uxTaskGetSystemState( pxTaskStatusArray, uxArraySize, &ulTotalTime );
+    vPortGetHeapStats( pxHeapStatus );
 
     /* todo: unnecessary opaque type within the same file. pass in the right type of pointer. */
     IotSerializerEncoderObject_t * pMetricsObject = ( IotSerializerEncoderObject_t * ) param1;
@@ -504,18 +514,49 @@ static void _serializeKernelRuntimeStats( void * param1 )
     serializerError = _defenderEncoder.openContainerWithKey( pMetricsObject,
                                                              KERNEL_METIRCS,
                                                              &mcuUptimeMap,
-                                                             7 ); /* define macro */
+                                                             12 ); /* define macro */
     assertNoError( serializerError );
 
-    /* free_heap_size, heap_low_watermark. */
+    /* heap_free_size
+     * heap_largest_free_block
+     * heap_smallest_free_block
+     * heap_free_blocks
+     * heap_low_watermark
+     * heap_succ_alloc
+     * heap_succ_free */
     serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
-                                                       KERNEL_FREE_HEAP_SIZE,
-                                                       IotSerializer_ScalarSignedInt( xPortGetFreeHeapSize() ) );
+                                                       KERNEL_HEAP_FREE_SIZE,
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xAvailableHeapSpaceInBytes ) );
+    assertNoError( serializerError );
+
+    serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
+                                                       KERNEL_HEAP_LARGEST_FREE,
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xSizeOfLargestFreeBlockInBytes ) );
+    assertNoError( serializerError );
+
+    serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
+                                                       KERNEL_HEAP_SMALLEST_FREE,
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xSizeOfSmallestFreeBlockInBytes ) );
+    assertNoError( serializerError );
+
+    serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
+                                                       KERNEL_HEAP_NUM_OF_FREE,
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xNumberOfFreeBlocks ) );
     assertNoError( serializerError );
 
     serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
                                                        KERNEL_HEAP_LOW_WATERMARK,
-                                                       IotSerializer_ScalarSignedInt( xPortGetMinimumEverFreeHeapSize() ) );
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xMinimumEverFreeBytesRemaining ) );
+    assertNoError( serializerError );
+
+    serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
+                                                       KERNEL_HEAP_SUCCESSFUL_ALLOC,
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xNumberOfSuccessfulAllocations ) );
+    assertNoError( serializerError );
+
+    serializerError = _defenderEncoder.appendKeyValue( &mcuUptimeMap,
+                                                       KERNEL_HEAP_SUCCESSFUL_FREE,
+                                                       IotSerializer_ScalarSignedInt( pxHeapStatus->xNumberOfSuccessfulFrees ) );
     assertNoError( serializerError );
 
     /* num_of_tasks. */
@@ -622,7 +663,8 @@ static void _serializeKernelRuntimeStats( void * param1 )
     assertNoError( serializerError );
 
     /* Free memory. */
-    vPortFree(pxTaskStatusArray);
+    vPortFree( pxTaskStatusArray );
+    vPortFree( pxHeapStatus );
 }
 
 /*-----------------------------------------------------------*/
