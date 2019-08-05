679c080a8bf5f0a0f655d352fe2bd59128b14169
Yuhui Zheng <10982575+yuhui-zheng@users.noreply.github.com>
2019-08-05 15:25:39 -0700

    clean up: separate scehma definition from source file.

--- libraries/c_sdk/aws/defender/src/aws_iot_defender_collector.c
+++ libraries/c_sdk/aws/defender/src/aws_iot_defender_collector.c
@@ -33,55 +33,14 @@
 
 #include "platform/iot_clock.h"
 
+#include "iot_defender_schema_v1.h"
+
 /* FreeRTOS specific. -- todo: this file should not take dependency on FreeRTOS.  */
 #include "task.h"
 
 /* Used in debugging. It will decode the report with cbor format and print to stdout. */
 #define DEBUG_CBOR_PRINT                0
 
-/* Long tags are not recommended to be used. */
-#define  HEADER_TAG                     AwsIotDefenderInternal_SelectTag( "header", "hed" )
-#define  REPORTID_TAG                   AwsIotDefenderInternal_SelectTag( "report_id", "rid" )
-#define  VERSION_TAG                    AwsIotDefenderInternal_SelectTag( "version", "v" )
-#define  VERSION_1_0                    "1.0"   /* Used by defender service to indicate the schema change of report, e.g. adding new field. */
-#define  VERSION_1_1                    "1.1"
-#define  METRICS_TAG                    AwsIotDefenderInternal_SelectTag( "metrics", "met" )
-
-/* Network metrics -- TCP connections. */
-#define TCP_CONN_TAG                    AwsIotDefenderInternal_SelectTag( "tcp_connections", "tc" )
-#define EST_CONN_TAG                    AwsIotDefenderInternal_SelectTag( "established_connections", "ec" )
-#define TOTAL_TAG                       AwsIotDefenderInternal_SelectTag( "total", "t" )
-#define CONN_TAG                        AwsIotDefenderInternal_SelectTag( "connections", "cs" )
-#define REMOTE_ADDR_TAG                 AwsIotDefenderInternal_SelectTag( "remote_addr", "rad" )
-
-/* Kernel metrics.
- * Prefixed with k (kernel), m (mcu), t (task), hp (hp), st (stack). */
-#define KERNEL_METRICS                  AwsIotDefenderInternal_SelectTag( "kernel_metrics", "km" )
-#define KERNEL_MCU_UPTIME               AwsIotDefenderInternal_SelectTag( "mcu_uptime", "mupt" )
-#define KERNEL_MCU_UTILIZATION          AwsIotDefenderInternal_SelectTag( "mcu_utilization", "mpct" )
-
-#define KERNEL_HEAP_FREE_SIZE           AwsIotDefenderInternal_SelectTag( "heap_free_size", "hp" )
-#define KERNEL_HEAP_LARGEST_FREE        AwsIotDefenderInternal_SelectTag( "heap_largest_free_block", "hplg" )
-#define KERNEL_HEAP_SMALLEST_FREE       AwsIotDefenderInternal_SelectTag( "heap_smallest_free_block", "hpsm" )
-#define KERNEL_HEAP_NUM_OF_FREE         AwsIotDefenderInternal_SelectTag( "heap_free_blocks", "hpno" )
-#define KERNEL_HEAP_LOW_WATERMARK       AwsIotDefenderInternal_SelectTag( "heap_low_watermark", "hplo" )
-#define KERNEL_HEAP_SUCCESSFUL_ALLOC    AwsIotDefenderInternal_SelectTag( "heap_succ_alloc", "hpal" )
-#define KERNEL_HEAP_SUCCESSFUL_FREE     AwsIotDefenderInternal_SelectTag( "heap_succ_free", "hpfr" )
-
-#define KERNEL_NUM_OF_TASKS             AwsIotDefenderInternal_SelectTag( "num_of_tasks", "tno" )
-#define KERNEL_TASK_DETAILS             AwsIotDefenderInternal_SelectTag( "task_details", "tdet" )
-
-#define KERNEL_TASK_ID                  AwsIotDefenderInternal_SelectTag( "task_id", "tid" )
-#define KERNEL_TASK_NAME                AwsIotDefenderInternal_SelectTag( "task_name", "tn" )
-#define KERNEL_TASK_STATUS              AwsIotDefenderInternal_SelectTag( "task_status", "ts" )
-#define KERNEL_TASK_PRIORITY            AwsIotDefenderInternal_SelectTag( "task_priority", "tp" )
-#define KERNEL_TASK_ABS_CYCLES          AwsIotDefenderInternal_SelectTag( "task_abs_cycles", "tcyc" )
-#define KERNEL_TASK_PERCENTAGE          AwsIotDefenderInternal_SelectTag( "task_percentage", "tpct" )
-#define KERNEL_STACK_HIGH_WATERMARK     AwsIotDefenderInternal_SelectTag( "stack_high_watermark", "sthi" )
-
-/* Device type.
- * Prefixed with d (device). */
-#define DEVICE_TYPE                     AwsIotDefenderInternal_SelectTag( "device_type", "dtp" )
 
 /* Magic numbers. */
 #if ( configDefenderReportHeapStats == 1 )
